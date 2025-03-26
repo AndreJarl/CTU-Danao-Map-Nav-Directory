24c6830110dc9e4b51f0e0e292b36c59eebba2e2
Andre Jarl <andrejarlaniana@gmail.com>
2025-03-26 23:05:53 +0800

    made home.jsx a landing page, and added map routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./index.css"
 import Home from "./Home"
 import {Route, Routes} from "react-router-dom"
 import EngBuilding from "./api/engineering/EngBuilding"
-
+import Map from './routes/Map'
 function App() {
      return(
       <>
@@ -10,7 +10,7 @@ function App() {
          <Routes>
            <Route path="/" element={<Home/>} />
            <Route path="/eng" element={<EngBuilding/>} />
-
+           <Route path="/map" element={<Map />} />
          </Routes>
          </>
      )
