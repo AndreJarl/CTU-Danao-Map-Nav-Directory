a62325d3bc838d29efd25750deeeaa855ed28674
xAkihiro666 <akihiroakio47@gmail.com>
2025-05-02 03:46:51 +0800

    Fixed text highlightings + Closes the panel when searching

--- src/routes/Map.jsx
+++ src/routes/Map.jsx
@@ -158,10 +158,16 @@ function Home() {
         setQuery([]);
         setCurrentFloor(1);
     }
+
+    const closePopUp = () => {
+        setShowPopup(false);
+        setQuery([]);
+        setCurrentFloor(1);
+      }
   
     return (
         <>
-        <div className="flex justify-center items-center overflow-hidden">
+        <div className="flex justify-center items-center overflow-hidden select-none">
 
       {/* this the svg div */}
       <div className="relative bg-white overflow-hidden h-screen w-screen flex justify-center items-center cursor-grab active:cursor-grabbing lg:h-screen lg:w-screen"
@@ -615,7 +621,7 @@ function Home() {
         <div className="fixed top-3  group right lg:right-6 2xl:right-96 px-2" >
             <div className=" rounded-lg shadow-xl shadow-slate-400 w-[300px] lg:w-[400px] border border-gray-300 px-5 flex flex-row justify-center items-center bg-white hover:rounded-t-xl">
             <input className="px-2 py-3 bg-white w-[200px] lg:w-[450px] border-none outline-none text-sm overflow-hidden" type="search" name="" id="" placeholder="Search building/facilities"
-            
+            onClick={() => closePopUp()}
             onChange={handleInputChange}
             />
             <div className="flex gap-2"> 
